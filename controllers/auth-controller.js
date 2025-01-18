70bfeeddf4fe94c4bc21fa97677ff573e49f2ab1
SomayPawa <lalpawasomay@gmail.com>
2025-01-18 15:07:49 +0530

    use schema

--- controllers/auth-controller.js
+++ controllers/auth-controller.js
@@ -1,3 +1,4 @@
+const User = require("../models/user-model");
 const home = async(req,res) => {
     try{
         res.status(200).send("hello babu baccha ye aapka hai home page");
@@ -10,7 +11,17 @@ const register = async(req,res)=>{
     try{
         console.log(req.body);
         // res.status(200).send("hello babu baccha ye aapka hai register page");
-        res.status(200).json({message:req.body});
+        const {username,email,phone,password} = req.body;
+        
+        const userExist = await User.findOne({email : email});
+
+        if(userExist){
+            return res.status(400).json({msg : "email already exists"});
+        }
+
+        await User.create({username,email,phone,password});
+
+        res.status(201).json({message:"user registered successfully"});
     }
     catch(error){
         console.log(error);
